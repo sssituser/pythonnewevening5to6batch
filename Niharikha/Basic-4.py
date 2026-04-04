62abcbd4b30f613c5d0898592d0dbb7e89f31430
arun <allamarunkumar@gmail.com>
2026-04-04 18:00:44 +0530

    updated

---
+++ Niharikha/Basic-4.py
@@ -0,0 +1,13 @@
+x = '10'
+print(f'x = {x} and its type is {type(x)}')
+x = 8
+print(f'x = {x} and its type is {type(x)}')
+x = 8.7
+print(f'x = {x} and its type is {type(x)}')
+x = True
+print(f'x = {x} and its type is {type(x)}')
+x = 8+9j
+print(f'x = {x} and its type is {type(x)}')
+
+
+
